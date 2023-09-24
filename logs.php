d70433e52d200fcd30d98ff94e0819cfd815f09d
Maquiz1 <manquiz92@gmail.com>
2023-09-24 13:17:54 +0300

    fix six months lab updates21

--- logs.php
+++ logs.php
@@ -36,12 +36,12 @@ if ($user->isLoggedIn()) {
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1>RECRUITMENTS STATUS</h1>
+                            <h1>RECRUITMENTS LOG STATUS</h1>
                         </div>
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="dashboard.php">Home</a></li>
-                                <li class="breadcrumb-item active">RECRUITMENTS STATUS</li>
+                                <li class="breadcrumb-item active">RECRUITMENTS LOG STATUS</li>
                             </ol>
                         </div>
                     </div>

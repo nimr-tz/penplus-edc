8a7c4724bb65361c648b72fad8d4c6a75de99a57
Maquiz1 <manquiz92@gmail.com>
2024-03-14 05:34:57 +0300

    fix GRAPHS

--- myjs/add/risks/risk_art.js
+++ myjs/add/risks/risk_art.js
@@ -1,26 +1,29 @@
-const risk_art = document.getElementById("risk_art");
-const risk_art_date = document.getElementById("risk_art_date");
+const risk_art1 = document.getElementById(`risk_art1`);
+const risk_art2 = document.getElementById(`risk_art2`);
 
-function showElement() {
-  if (risk_art.value === "1") {
+const risk_art_date_label = document.getElementById(`risk_art_date_label`);
+const risk_art_date = document.getElementById(`risk_art_date`);
+
+function toggleElementVisibility() {
+  if (risk_art1.checked) {
+    risk_art_date_label.style.display = "block";
     risk_art_date.style.display = "block";
+    risk_art_date.setAttribute("required", "required");
   } else {
+    risk_art_date_label.style.display = "none";
     risk_art_date.style.display = "none";
+    risk_art_date.removeAttribute("required");
   }
-
-  // Save the selected value in localStorage
-  localStorage.setItem("selectedValue", risk_art.value);
 }
+// Initial check
+toggleElementVisibility();
 
-// Check if there's a previously selected value in localStorage
-const risk_artValue = localStorage.getItem("selectedValue");
+risk_art2.addEventListener("change", toggleElementVisibility);
+risk_art2.addEventListener("change", toggleElementVisibility);
 
-if (risk_artValue) {
-  risk_art.value = risk_artValue;
+function unsetRiskArt() {
+  var unsetRiskArts = document.getElementsByName("risk_art");
+  unsetRiskArts.forEach(function (unsetRiskArt) {
+    unsetRiskArt.checked = false;
+  });
 }
-
-// Show element if Option 2 is selected
-showElement();
-
-// Listen for changes in the dropdown
-risk_art.addEventListener("change", showElement);
--- myjs/add/risks/risk_hiv.js
+++ myjs/add/risks/risk_hiv.js
@@ -1,32 +1,36 @@
-const risk_hiv = document.getElementById("risk_hiv");
-const risk_hiv_date = document.getElementById("risk_hiv_date");
-const risk_art1 = document.getElementById("risk_art1");
+const risk_hiv1 = document.getElementById(`risk_hiv1`);
+const risk_hiv2 = document.getElementById(`risk_hiv2`);
+const risk_hiv3 = document.getElementById(`risk_hiv3`);
 
-function showElement() {
-  if (risk_hiv.value === "1") {
+const risk_hiv_date = document.getElementById(`risk_hiv_date`);
+const risk_hiv_date_label = document.getElementById(`risk_hiv_date_label`);
+const risk_art1_1 = document.getElementById(`risk_art1`);
+const risk_art = document.getElementById(`risk_art`);
+
+function toggleElementVisibility() {
+  if (risk_hiv1.checked) {
+    risk_hiv_date_label.style.display = "block";
     risk_hiv_date.style.display = "block";
-    risk_art1.style.display = "block";
-  } else if (risk_hiv.value === "2") {
+    risk_hiv_date.setAttribute("required", "required");
+    risk_art.style.display = "block";
+    risk_art1_1.setAttribute("required", "required");
+  } else if (risk_hiv2.checked) {
+    risk_hiv_date_label.style.display = "block";
     risk_hiv_date.style.display = "block";
-    risk_art1.style.display = "none";
+    risk_hiv_date.setAttribute("required", "required");
+    risk_art.style.display = "none";
+    risk_art1_1.removeAttribute("required");
   } else {
+    risk_hiv_date_label.style.display = "block";
     risk_hiv_date.style.display = "none";
-    risk_art1.style.display = "none";
+    risk_hiv_date.removeAttribute("required");
+    risk_art.style.display = "none";
+    risk_art1_1.removeAttribute("required");
   }
-
-  // Save the selected value in localStorage
-  localStorage.setItem("selectedValue", risk_hiv.value);
 }
+// Initial check
+toggleElementVisibility();
 
-// Check if there's a previously selected value in localStorage
-const risk_hivValue = localStorage.getItem("selectedValue");
-
-if (risk_hivValue) {
-  risk_hiv.value = risk_hivValue;
-}
-
-// Show element if Option 2 is selected
-showElement();
-
-// Listen for changes in the dropdown
-risk_hiv.addEventListener("change", showElement);
+risk_hiv1.addEventListener("change", toggleElementVisibility);
+risk_hiv2.addEventListener("change", toggleElementVisibility);
+risk_hiv3.addEventListener("change", toggleElementVisibility);
